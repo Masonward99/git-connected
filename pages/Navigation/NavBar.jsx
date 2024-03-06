e6ed33016b53a9f163e192cc58aeedbf7f0dffcb
Mason <mason@Masons-MacBook-Air-2.local>
2024-03-06 15:29:24 +0000

    basic styling for nav

--- pages/Navigation/NavBar.jsx
+++ pages/Navigation/NavBar.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom"
 function NavBar() {
     return (
-        <ul>
-            <li>Home</li>
+        <ul className="NavBar">
+            <li id="navLogo">
+                <Link to='/'>
+                    <div>Home</div>
+                </Link>
+                
+            </li>
             <li>Users</li>
             <li>Projects</li>
             <li>Messages</li>
-            <li>Profile</li>
+            <li>
+                <Link to='/profile'>
+                    <div>Profile</div>
+                </Link>
+            </li>
         </ul>
     )
 }
